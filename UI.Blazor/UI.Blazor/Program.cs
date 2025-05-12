2f17ff8ff462c7348f4da34bdc1f1c59580998b9
ailci <ali.ilci@gmx.de>
2025-05-12 12:04:06 +0200

    Add project files.

---
+++ UI.Blazor/UI.Blazor/Program.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UI.Blazor.Client.Pages;
+using UI.Blazor.Components;
+using UI.Blazor.Components.Account;
+using UI.Blazor.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents() //Componenten
+    .AddInteractiveServerComponents()  //Blazor Server
+    .AddInteractiveWebAssemblyComponents() // Blazor WebAssembly
+    .AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. ##################################################
+//app.Use(async (context, next) =>
+//{
+//    var userAgent = context.Request.Headers["User-Agent"][0].ToLower();
+//    await context.Response.WriteAsync($"Erste middleware\n {userAgent}");
+//    await next();
+//    await context.Response.WriteAsync("Erste Back middleware\n");
+//});
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("Zweite middleware\n");
+//    await next();
+//    await context.Response.WriteAsync("Zweite Back middleware\n");
+//});
+//app.Run(async context =>
+//{
+//    await context.Response.WriteAsync("End middleware\n");
+//});
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(UI.Blazor.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
