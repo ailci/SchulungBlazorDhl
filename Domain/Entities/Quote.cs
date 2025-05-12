436d2a8528c75fcdadc49aa7efce3cb8219605f4
ailci <ali.ilci@gmx.de>
2025-05-12 14:35:27 +0200

    Domain

---
+++ Domain/Entities/Quote.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities;
+
+public class Quote
+{
+    //[Key]
+    public Guid Id { get; set; } //QuoteId, ID, Id
+    public required string QuoteText { get; set; }
+    public Author? Author { get; set; }
+    public Guid AuthorId { get; set; }
+}
