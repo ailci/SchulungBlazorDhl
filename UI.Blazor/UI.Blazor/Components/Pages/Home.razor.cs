24906fb8750ff7f3cd8681254ee843a21fa6fbcb
ailci <ali.ilci@gmx.de>
2025-05-13 12:40:13 +0200

    Prerendering

--- UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
@@ -6,14 +6,45 @@ using Persistence;
 
 namespace UI.Blazor.Components.Pages;
 
-public partial class Home
+public partial class Home : IDisposable
 {
     [Inject] public IServiceManager ServiceManager { get; set; } = null!;
+    [Inject] public PersistentComponentState ApplicationState { get; set; } = null!;
+    private PersistingComponentStateSubscription _persistingComponentStateSubscription;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
     private readonly string _color = "text-primary";
 
     protected override async Task OnInitializedAsync()
     {
-        QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+        //3. Lösung Herstellerempfehlung
+        _persistingComponentStateSubscription = ApplicationState.RegisterOnPersisting(PersistData);
+
+        if (!ApplicationState.TryTakeFromJson<QuoteOfTheDayViewModel>(nameof(QotdViewModel), out var restoredData))
+        {
+            //Hol aus DB
+            QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+        }
+        else
+        {
+            QotdViewModel = restoredData;
+        }
     }
+
+    private Task PersistData()
+    {
+        ApplicationState.PersistAsJson(nameof(QotdViewModel), QotdViewModel);
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() => _persistingComponentStateSubscription.Dispose();
+
+    //2. Lösung
+    //protected override async Task OnAfterRenderAsync(bool firstRender)
+    //{
+    //    if (firstRender)
+    //    {
+    //        QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+    //        StateHasChanged();
+    //    }
+    //}
 }
