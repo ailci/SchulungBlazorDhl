627485a04440ac5d56632558d0bec02079a41187
ailci <ali.ilci@gmx.de>
2025-05-13 10:26:05 +0200

    Migrations

--- UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/UI.Blazor/Components/Pages/Home.razor.cs
@@ -1,13 +1,18 @@
 using Application.ViewModels.Qotd;
+using Microsoft.AspNetCore.Components;
+using Persistence;
 
 namespace UI.Blazor.Components.Pages;
 public partial class Home
 {
+    [Inject] public QotdContext QotdContext { get; set; } = null!;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
     private readonly string _color = "text-primary";
 
     protected override void OnInitialized()
     {
+        var authors = QotdContext.Authors.ToList();
+
         QotdViewModel = new QuoteOfTheDayViewModel
         {
             AuthorName = "Ich",
