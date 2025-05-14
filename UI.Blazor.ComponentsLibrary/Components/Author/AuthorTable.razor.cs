94ca11d9c2c2c255674894d0ae6cb984a9c1faee
ailci <ali.ilci@gmx.de>
2025-05-14 12:27:43 +0200

    EventHandling

--- UI.Blazor.ComponentsLibrary/Components/Author/AuthorTable.razor.cs
+++ UI.Blazor.ComponentsLibrary/Components/Author/AuthorTable.razor.cs
@@ -7,13 +7,14 @@ public partial class AuthorTable
 {
     [Inject] public IJSRuntime JsRuntime { get; set; } = null!;
     [Parameter] public IEnumerable<AuthorViewModel>? AuthorViewModels { get; set; }
+    [Parameter] public EventCallback<Guid> OnAuthorDelete { get; set; }
 
     private async Task ShowConfirmDialog(AuthorViewModel authorVm)
     {
         //1. Version Klassik
         if (await JsRuntime.InvokeAsync<bool>("confirm", $"Wollen Sie wirklich den Autor '{authorVm.Name}' löschen?"))
         {
-            //TODO: Eltern benachrichtigen zum Löschen
+            await OnAuthorDelete.InvokeAsync(authorVm.Id);
         }
     }
 }
