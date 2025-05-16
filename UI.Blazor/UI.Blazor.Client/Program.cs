460e22a080a34d382a57ef56906f343c3a801f0c
ailci <ali.ilci@gmx.de>
2025-05-16 13:54:44 +0200

    AppSettings

--- UI.Blazor/UI.Blazor.Client/Program.cs
+++ UI.Blazor/UI.Blazor.Client/Program.cs
@@ -1,6 +1,7 @@
 using Application.Contracts.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Options;
+using UI.Blazor.Client;
 using UI.Blazor.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,8 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddAuthenticationStateDeserialization();
 
+//Options-Pattern => DI
+builder.Services.Configure<QotdAppSettings>(builder.Configuration.GetSection(nameof(QotdAppSettings)));
 builder.Services.AddScoped<IQotdService, QotdApiService>();
 
 //Named-Http-Client
@@ -18,10 +21,12 @@ builder.Services.AddScoped<IQotdService, QotdApiService>();
 //    options.DefaultRequestHeaders.Add("Accept", "application/json");
 //});
 
+var qotdAppSettings = builder.Configuration.GetSection(nameof(QotdAppSettings)).Get<QotdAppSettings>();
+
 //Typed Client
 builder.Services.AddHttpClient<IQotdService, QotdApiService>(client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7260");
+    client.BaseAddress = new Uri(qotdAppSettings!.QotdServiceApiUri);
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 });
 
--- UI.Blazor/UI.Blazor.Client/Services/QotdApiService.cs
+++ UI.Blazor/UI.Blazor.Client/Services/QotdApiService.cs
@@ -2,17 +2,19 @@
 using Application.ViewModels.Qotd;
 using System.Net.Http.Json;
 using System.Net.Http;
+using Microsoft.Extensions.Options;
 
 namespace UI.Blazor.Client.Services;
 
-public class QotdApiService(ILogger<QotdApiService> logger, HttpClient client) : IQotdService
+public class QotdApiService(ILogger<QotdApiService> logger, HttpClient client, IOptions<QotdAppSettings> appSettings) : IQotdService
 {
+    private readonly QotdAppSettings _appSettings = appSettings.Value;
     private const string QotdUri = "api/qotd";
 
     public async Task<QuoteOfTheDayViewModel> GetQuoteOfTheDayAsync()
     {
         logger.LogInformation($"{nameof(GetQuoteOfTheDayAsync)} aufgerufen...");
-
+        
         //var client = clientFactory.CreateClient("qotdapiservice");
         return await client.GetFromJsonAsync<QuoteOfTheDayViewModel>(QotdUri);
     }
