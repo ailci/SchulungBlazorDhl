7203ad3d77fd6b591f9889045a55b9808207561a
ailci <ali.ilci@gmx.de>
2025-05-16 09:30:10 +0200

    Profession

---
+++ Application/Profession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application;
+
+public enum Profession
+{
+    Trainee,
+    Apprentice,
+    Employee,
+    Supervisor,
+    Chief,
+    God
+}
